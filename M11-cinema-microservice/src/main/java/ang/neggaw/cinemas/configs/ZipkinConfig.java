c546ee7c12a0256c3a1efa7921ad333e0f61a45a
ANG <code4ang@gmail.com>
2021-06-18 21:15:20 +0200

    Configuration & implementation of Swagger (springfox)

---
+++ M11-cinema-microservice/src/main/java/ang/neggaw/cinemas/configs/ZipkinConfig.java
@@ -0,0 +1,14 @@
+package ang.neggaw.cinemas.configs;
+
+import brave.sampler.Sampler;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ZipkinConfig {
+
+    @Bean
+    @LoadBalanced
+    public Sampler defaultSampler() { return Sampler.ALWAYS_SAMPLE; }
+}
---
+++ M13-ticket-microservice/src/main/java/ang/neggaw/tickets/configs/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package ang.neggaw.tickets.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ang.neggaw.tickets.restControllers"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(ApiInfo.DEFAULT);
+    }
+}
