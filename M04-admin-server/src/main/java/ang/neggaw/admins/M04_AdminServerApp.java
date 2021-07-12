7351f3b2d81970f6350161236b672894cb1d3571
ANG <code4ang@gmail.com>
2021-07-12 19:13:57 +0200

    M04-admin-server: spring security

--- M04-admin-server/src/main/java/ang/neggaw/admins/M04_AdminServerApp.java
+++ M04-admin-server/src/main/java/ang/neggaw/admins/M04_AdminServerApp.java
@@ -4,6 +4,8 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @EnableAdminServer
@@ -12,4 +14,7 @@ public class M04_AdminServerApp {
     public static void main(String[] args) {
         SpringApplication.run(M04_AdminServerApp.class, args);
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 }
